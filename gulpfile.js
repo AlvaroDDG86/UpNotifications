ee151fe2d517a769d31a0995bac9816ec5af09bc
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2017-11-25 14:02:21 +0100

    agregando browser-sync al proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['sass'], function() {
+        browserSync.init({
+            server: "."
+        });
+        gulp.watch("scss/*.scss", ['sass']);
+        gulp.watch("*.html").on('change', browserSync.reload);
+    });
 
 gulp.task('sass', function(){
-    return gulp.src('scss/upnotification.scss')
+    console.log("Procesando scss");
+    return gulp.src('scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(autoprefixer({
+            versions:['last 2 browsers']
+        }))
+        .pipe(browserSync.stream());
 });
 
 
 gulp.task('default', function(){
-    gulp.watch('scss/upnotification.scss',['sass']);
+    gulp.watch('scss/*.scss',['sass']);
 });
